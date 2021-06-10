32890c2b1d528616672073cfd5bf0106f0e01ba7
Hohnn <jean-bateee@hotmail.fr>
2021-06-10 23:45:32 +0200

    menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,19 @@
-let test = '4'
-console.log(Number.isInteger(+test / 2))
+const userName = document.getElementById('userName')
+const profilLogo = document.getElementById('profilLogo')
+const profilMenu = document.querySelector('.profilMenu')
+
+function toggleMenu (target) {
+    target.addEventListener('click', () => {
+        profilMenu.classList.toggle('focused')
+    })
+}
+function removeMenu (target) {
+    target.addEventListener('focusout', () => {
+        profilMenu.classList.remove('focused')
+    })
+}
+
+toggleMenu(userName)
+removeMenu(userName)
+toggleMenu(profilLogo)
+removeMenu(profilLogo)
