ac676a81c4fe06be637eec56e2a491c1a9c3d33c
Hohnn <jean-bateee@hotmail.fr>
2021-08-15 18:57:57 +0200

    add about style

--- about.php
+++ about.php
@@ -12,18 +12,118 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="./assets/sass/style.css">
-    <link rel="stylesheet" href="./assets/sass/comp.css">
+    <link rel="stylesheet" href="./assets/sass/about.css">
     <title>DAW esport</title>
 </head>
 <?php include './components/header.php' ?>
-                <section class="team">
-                    <h1 class="text-uppercase">équipes</h1>
+                <section class="about" id="about">
+                    
                     <div class="container-fluid  mt-5">
                         <div class="row g-3">
                             <div class="col " >
-                  
+                                <div class="header">
+                                    <h1><span>DAW</span> eSport</h1>
+                                    <div class="subtitle">Une équipe compétitive mais pas que ...</div>
+                                    <p class="tag">Rester informé sur l'actualité <br> et la compétition sur Battlefield</p>
+                                    <a href="login.php" class="btn bgYellow text-white">Créer un compte</a>
+                                <img class="floatImg" src="./assets/images/twisted.png" alt="twisted">
+                                    
                             </div>
-                        
+                        </div>
+                    </div>
+                    <div class="fullImg">
+                        <h2>L'essentiel est regroupé pour un meilleur suivi !</h2>
+                        <p>les compétions eSport sont souvent difficile à suivre, 
+                        les matchs et événements sont la plupart du temps organisé 
+                        sur différente plate-forme</p>
+                    </div>
+                    <div class="container-fluid  mt-5">
+                        <div class="row g-3">
+                            <div class="col " >
+                                <div class="descCard myCard">
+                                    <div class="icon">
+                                        <i class="bi bi-newspaper"></i>
+                                    </div>
+                                    <h4>L'actualité</h4>
+                                    <p>Nous sélectionnons 3 articles qui nous semble pertinent pour les fans de la franchise</p>
+                                </div>
+                            </div>
+                            <div class="col " >
+                                <div class="descCard myCard">
+                                    <div class="icon">
+                                        <i class="bi bi-twitch"></i>
+                                    </div>
+                                    <h4>Twitch</h4>
+                                    <p>Découvrez ou redécouvrez les streamers en live actuellement sur le jeu</p>
+                                </div>
+                            </div>
+                            <div class="col " >
+                                <div class="descCard myCard">
+                                    <div class="icon">
+                                        <i class="bi bi-youtube"></i>
+                                    </div>
+                                    <h4>Youtube</h4>
+                                    <p>Une sélection du meilleur contenu créer par la communauté pour les joueurs</p>
+                                </div>
+                            </div>
+                            <div class="col " >
+                                <div class="descCard myCard">
+                                    <div class="icon">
+                                        <i class="bi bi-shield-shaded"></i>
+                                    </div>
+                                    <h4>équipes</h4>
+                                    <p>Les équipes les plus active seront répertorié avec leur joueurs les plus actifs</p>
+                                </div>
+                            </div>
+                            <div class="col " >
+                                <div class="descCard myCard">
+                                    <div class="icon">
+                                        <i class="bi bi-window-sidebar"></i>
+                                    </div>
+                                    <h4>Tournois</h4>
+                                    <p>Un aperçu des derniers tournois répertorié avec les équipes qui y participe</p>
+                                </div>
+                            </div>
+                            <div class="col " >
+                                <div class="descCard myCard">
+                                    <div class="icon">
+                                        <i class="bi bi-window-sidebar"></i>
+                                    </div>
+                                    <h4>Matchs</h4>
+                                    <p>Les derniers matchs avec toutes les informations et la possibilité de revoir le match</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row g-3">
+                            <div class="col " >
+                                <img class="floatImg" src="./assets/images/twisted.png" alt="twisted">
+                                <div class="step">
+                                    <h3>Faite vous connaitre !</h3>
+                                    <div class="wrap">
+                                        <div class="number">
+                                            <span>01</span>
+                                        </div>
+                                        <div class="title">Créer votre compte</div>
+                                        <div class="desc">Indispensable pour profiter des déférentes fonctionnalités</div>
+                                    </div>
+                                    <div class="wrap">
+                                        <div class="number">
+                                            <span>02</span>
+                                        </div>
+                                        <div class="title">Personnalisé votre profil</div>
+                                        <div class="desc">Ajouter  votre compte de jeu et vos réseaux sociaux pour une expérience optimale</div>
+                                    </div>
+                                    <div class="wrap">
+                                        <div class="number">
+                                            <span>03</span>
+                                        </div>
+                                        <div class="title">Profiter d'une visibilité accru</div>
+                                        <div class="desc">Votre équipe, vos matchs, vos states et vos réseaux sont maintenant regroupé et visible de tous</div>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                            
                         </div>
                     </div>    
                 </section>
@@ -33,40 +133,6 @@
     </div>
 
     
-Ce site groupe toutes les informations nécessaire pour suivre l'actualité et la compétition sur la franchise Battlefield,
-
-Dans la partie actualité nous sélectionnons 3 articles qui nous semble pertinent pour les joueurs, les membres peuvent nous proposé des articles qu'ils trouvent intéressent.
-
-Les chaînes twitch en lives sur le jeu sont affiché ainsi que les dernières vidéos youtube de la communauté, ce qu'ils leur permettra d'avoir une visibilité supplémentaire et de découvrir de nouvelle personnes.
-
-Une partie membre affiche tous les membres inscrit sur le site, un click redirige vers leur profil.
-
-La page profil joue un rôle important, elle permet au joueurs qui n'ont souvent pas la visibilité qu'il mérite de ce faire connaître, ils peuvent afficher leur states en jeu ainsi que leur réseaux sociaux.
-Les states sont récupéré depuis le site battlefieldtracker.com, cela permet d’accéder à leur profil rapidement sans devoir chercher leur identifiant origin.
-
-la sections Compétitions :
-
-les compétions eSport sur le jeu vidéo Battlefield sont souvent difficile à suivre, les matchs et événements qui sont organisé sont la plupart du temps répartie sur différente plate-forme, les matchs s'organise souvent à la dernière minutes suivant les disponibilités des joueurs des 2 équipes,
-
-Ce site regroupe toutes les informations dont les joueurs et supporters ont besoin pour suivre les matchs et tournois de leur équipe ou équipe favorite, Cela permet d'avoir un coté plus visuel sur l'activité eSport de Battlefield
-
-	les équipes
-Les équipes les plus active seront répertorier avec leur joueurs les plus actifs
-Cela peut permettre aux équipes et joueurs de ce faire connaître 
-Si les joueurs sont inscrit sur le site on auras la possibilité d'aller voir leur profils
-
-	les tournois
-Les derniers tournois seront répertorier avec les équipes qui y participe
-Un click redirige directement vers la page du tournoi officiel
-
-	Les matchs récents
-Les derniers matchs seront répertorier avec toutes les informations indispensable
-les 2 équipes, le score, les cartes, le tournoi, la date, la VOD
-Un click redirige vers la vidéo pour revoir le match
-
-	Les casters 
-Les casters actifs seront répertorier
-Un click redirige vers leur chaine twitch 
 
 
     <!-- Script -->
