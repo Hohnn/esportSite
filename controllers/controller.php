a69119609b29bdd9290f9daadce2164dc2317792
unknown <jean-bateee@hotmail.fr>
2021-08-25 16:52:57 +0200

    prépa calss

---
+++ controllers/controller.php
@@ -0,0 +1,5 @@
+<?php 
+require './database.php';
+require '../models/user-model.php'
+
+?>
---
+++ models/user-model.php
@@ -0,0 +1,107 @@
+<?php 
+class Appointments extends database {
+
+    public function setRdv ($datehour, $idPatient){
+        $bdd = $this->connectDatabase();
+        $condition = "INSERT INTO appointments (datehour, idPatients)
+        VALUES (?, ?)";
+        $result = $bdd->prepare($condition);
+        $result->bindValue(1, $datehour, PDO::PARAM_STR);
+        $result->bindValue(2, $idPatient, PDO::PARAM_INT);
+        $result->execute();
+        return $result;
+    }
+
+    public function getRdv (){
+        $bdd = $this->connectDatabase();
+        $condition = "SELECT  `appointments`.`id`, `idPatients`, `firstname`, `lastname`, DATE_FORMAT(`datehour`, '%d/%m/%Y %H:%i') as datehour FROM appointments
+        inner join patients on patients.id = appointments.idPatients order by datehour";
+        $result = $bdd->query($condition)->fetchAll();
+        return $result;
+    }
+
+    public function deleteRdv ($id){
+        $bdd = $this->connectDatabase();
+        $condition = "DELETE FROM appointments WHERE id = ?";
+        $result = $bdd->prepare($condition);
+        $result->bindValue(1, $id, PDO::PARAM_INT);
+        $result->execute();
+    }
+
+    public function getRdvInfo($id){
+        $condition = "SELECT  `appointments`.`id`, `idPatients`, `firstname`, `lastname`, `datehour` FROM appointments
+        inner join patients on patients.id = appointments.idPatients WHERE appointments.id = ?";
+        $bdd = $this->connectDatabase();
+        $result = $bdd->prepare($condition);
+        $result->bindValue(1, $id, PDO::PARAM_INT);
+        $result->execute();
+        $fetch = $result->fetch(); /* pas fetchAll pour pas avoir 2 array */
+        return $fetch;
+    }
+
+    public function updateRdv($id, $datehour, $idPatients){
+        $condition = "UPDATE appointments SET datehour = ?, idPatients = ? WHERE id = ?";
+        $bdd = $this->connectDatabase();
+        $result = $bdd->prepare($condition);
+        $result->bindValue(1, $datehour, PDO::PARAM_STR);
+        $result->bindValue(2, $idPatients, PDO::PARAM_INT);
+        $result->bindValue(3, $id, PDO::PARAM_INT);
+        $result->execute();
+        return $result;
+    }
+
+    public function getRdvByPatient($idPatient){
+        $condition = "SELECT  `appointments`.`id`, `idPatients`, `firstname`, `lastname`, DATE_FORMAT(`datehour`, '%d/%m/%Y %h:%i') as datehour FROM appointments
+        inner join patients on patients.id = appointments.idPatients WHERE appointments.idPatients = ? order by datehour";
+        $bdd = $this->connectDatabase();
+        $result = $bdd->prepare($condition);
+        $result->bindValue(1, $idPatient, PDO::PARAM_INT);
+        $result->execute();
+        $fetch = $result->fetchAll();
+        return $fetch;
+    }
+
+    public function checkSameRdv($datehour){
+        $condition = "SELECT * FROM appointments
+        where dateHour = ' ? '";
+        $bdd = $this->connectDatabase();
+        $result = $bdd->prepare($condition);
+        $result->bindValue(1, $datehour, PDO::PARAM_STR);
+        $result->execute();
+        $fetch = $result->fetch(); /* pas fetchAll pour pas avoir 2 array */
+        return $fetch;
+    }
+
+
+    public function deletePatientRdv($idPatient){
+        $condition = "DELETE FROM appointments WHERE idPatients = ? ";
+        $bdd = $this->connectDatabase();
+        $result = $bdd->prepare($condition);
+        $result->bindValue(1, $idPatient, PDO::PARAM_INT);
+        $result->execute();
+    }
+
+
+}
+
+class User extends database {
+
+    public function setUser($username, $mail, $password) {
+        $bdd = $this->connectDatabase();
+        $condition = "INSERT INTO user (username, mail, password)
+        VALUES (?, ?, ?)";
+        $result = $bdd->prepare($condition);
+        $result->bindValue(1, $username, PDO::PARAM_STR);
+        $result->bindValue(2, $mail, PDO::PARAM_STR);
+        $result->bindValue(3, $password, PDO::PARAM_STR);
+        $result->execute();
+        return $result;
+    }
+
+    public function getAllUser() {
+        $bdd = $this->connectDatabase();
+        $condition = "SELECT * FROM user ";
+        
+    }
+
+}
